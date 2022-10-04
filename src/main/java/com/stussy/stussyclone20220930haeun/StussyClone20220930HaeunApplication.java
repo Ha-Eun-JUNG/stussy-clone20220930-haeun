98c8924527f66dcef21b15275c35937a736c4b6f
LAPTOP-2LJ4MLK1\SAMSUNG <jhe4817@gmail.com>
2022-10-04 14:42:32 +0900

    2022-09-30 쇼핑몰 클론코딩하기

---
+++ src/main/java/com/stussy/stussyclone20220930haeun/StussyClone20220930HaeunApplication.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220930haeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StussyClone20220930HaeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StussyClone20220930HaeunApplication.class, args);
+	}
+
+}
